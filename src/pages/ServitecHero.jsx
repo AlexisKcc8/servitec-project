5db5b5973e2ba0a3c049a168e56637cf8301202b
AlexisKcc8 <CodeKJS863@gmail.com>
2024-11-11 13:19:54 -0600

    remove component oberver element in hero

--- src/pages/ServitecHero.jsx
+++ src/pages/ServitecHero.jsx
@@ -1,7 +1,6 @@
 import { StoreSvg } from "@/myComponents/icons/StoreSvg";
 import { ArrowDown } from "@/myComponents/icons/ArrowDown";
 import { PictureImage } from "@/myComponents/PictureImage";
-import { ObserverElement } from "@/myComponents/ObserverElement";
 export const ServitecHero = () => {
   return (
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-black text-white">
@@ -29,44 +28,35 @@ export const ServitecHero = () => {
 
       {/* Content */}
       <header className="relative z-10 text-center px-4 sm:px-6 lg:px-8 max-w-5xl overflow-hidden">
-        <ObserverElement animateIn="animate-fade-up">
-          <div className="border border-white rounded-lg title-page-main relative ">
-            <h1 className="text-[3.8rem] sm:text-5xl md:text-6xl laptop:text-[10rem] font-bold tracking-tight sm:mb-6 text-white [text-shadow:0_0_20px_rgba(255,255,255,0.3)]">
-              SERVITEC
-            </h1>
-          </div>
-        </ObserverElement>
+        <div className="border border-white rounded-lg title-page-main relative ">
+          <h1 className="text-[3.8rem] sm:text-5xl md:text-6xl laptop:text-[10rem] font-bold tracking-tight sm:mb-6 text-white [text-shadow:0_0_20px_rgba(255,255,255,0.3)]">
+            SERVITEC
+          </h1>
+        </div>
 
-        <ObserverElement animateIn="animate-fade-up">
-          <h2 className=" text-[2rem] sm:text-5xl md:text-6xl laptop:text-[3rem] font-bold tracking-tight mb-4 sm:mb-6 text-white [text-shadow:0_0_20px_rgba(255,255,255,0.4)]">
-            REPARACIÓNES RÁPIDAS, SONRISAS GARANTIZADAS.
-          </h2>
-        </ObserverElement>
+        <h2 className=" text-[2rem] sm:text-5xl md:text-6xl laptop:text-[3rem] font-bold tracking-tight mb-4 sm:mb-6 text-white [text-shadow:0_0_20px_rgba(255,255,255,0.4)]">
+          REPARACIÓNES RÁPIDAS, SONRISAS GARANTIZADAS.
+        </h2>
 
-        <ObserverElement animateIn="animate-fade-left">
-          <p className="text-base sm:text-lg md:text-3xl lg:text-4xl mb-6 sm:mb-8 max-w-3xl mx-auto text-gray-200 [text-shadow:0_0_10px_rgba(255,255,255,0.2)]">
-            Expertos en reparación de dispositivos móviles y computadoras.
-            Soluciones rápidas y confiables para mantener tu tecnología
-            funcionando sin problemas.
-          </p>
-        </ObserverElement>
-
-        <ObserverElement animateIn="animate-fade-down">
-          <div className="animate-delay-150 animate-fill-forwards flex flex-col laptop:flex-row justify-center items-center space-y-4 laptop:space-y-0 laptop:space-x-4">
-            <a
-              href="#"
-              className="animation-fill-mode: forwards bg-white flex text-black hover:bg-gray-200 text-base sm:text-lg px-6 sm:px-8 py-2 laptop:py-3 rounded-md transition-all duration-300 w-auto"
-            >
-              <StoreSvg /> Mirar Productos
-            </a>
-            <a
-              href="#our-services"
-              className="animation-fill-mode: forwards bg-transparent flex text-white border border-white hover:bg-gray-200 text-base sm:text-lg px-6 sm:px-8 py-2 laptop:py-3 rounded-md transition-all duration-300 w-auto"
-            >
-              <ArrowDown /> Servicios
-            </a>
-          </div>
-        </ObserverElement>
+        <p className="text-base sm:text-lg md:text-3xl lg:text-4xl mb-6 sm:mb-8 max-w-3xl mx-auto text-gray-200 [text-shadow:0_0_10px_rgba(255,255,255,0.2)]">
+          Expertos en reparación de dispositivos móviles y computadoras.
+          Soluciones rápidas y confiables para mantener tu tecnología
+          funcionando sin problemas.
+        </p>
+        <div className="animate-delay-150 animate-fill-forwards flex flex-col laptop:flex-row justify-center items-center space-y-4 laptop:space-y-0 laptop:space-x-4">
+          <a
+            href="#"
+            className="animation-fill-mode: forwards bg-white flex text-black hover:bg-gray-200 text-base sm:text-lg px-6 sm:px-8 py-2 laptop:py-3 rounded-md transition-all duration-300 w-auto"
+          >
+            <StoreSvg /> Mirar Productos
+          </a>
+          <a
+            href="#our-services"
+            className="animation-fill-mode: forwards bg-transparent flex text-white border border-white hover:bg-gray-200 text-base sm:text-lg px-6 sm:px-8 py-2 laptop:py-3 rounded-md transition-all duration-300 w-auto"
+          >
+            <ArrowDown /> Servicios
+          </a>
+        </div>
       </header>
     </section>
   );
